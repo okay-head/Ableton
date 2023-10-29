2b024ab1fa17263d5ad06fabf8a7d0a4df1e45ba
okay-head <68184623+okay-head@users.noreply.github.com>
2023-10-29 12:23:35 +0530

    added jquery locally | updated deprecated syntax

--- script.js
+++ script.js
@@ -1,32 +1,15 @@
-'use strict';
+'use strict'
 
 //navbar
-$('.mobile-dropdown').hide();
-// $('.menu').hide();
+$('.mobile-dropdown').hide()
 
-$('.menu-btn').focus(()=>{
-   preventDefault();
+$('.menu-btn').on('focus', () => {
+	preventDefault()
 })
-$('.menu-btn').click(()=>{
-   $('.mobile-dropdown').slideToggle(600)
-   $('.main').toggleClass('blue-bg')
-   // setTimeout(() => {
-      
-   // }, 1000);
-
-   // $('.menu').slideToggle(1100)
-   // $('.fall1').slideToggle(1200)
-   // $('.fall2').slideToggle(600)
-   // $('.fall3').slideToggle(900)
-   // $('.menu').slideToggle(300)
-   // $('.fall2').slideToggle(800)
-   // $('.fall3').slideToggle(1200)
-
-
-   
+$('.menu-btn').on('click', () => {
+	$('.mobile-dropdown').slideToggle(600)
+	$('.main').toggleClass('blue-bg')
 })
-
-
 
 // --- hide show ---
 const intro = document.querySelector('.intro')
@@ -37,32 +20,26 @@ const section4 = document.querySelector('.section-4')
 const section5 = document.querySelector('.section-5')
 
 // collections
-const array = [intro,section1,section2,section3,section4,section5];
-
+const array = [intro, section1, section2, section3, section4, section5]
 
 function offset(element) {
-   const x = Math.round(window.scrollY + element.getBoundingClientRect().top);
-   return x;
+	const x = Math.round(window.scrollY + element.getBoundingClientRect().top)
+	return x
 }
 
-/* function show(element) {
-   $(element.toString()).show();
-   console.log(element)
-} */
-
 // initialization
-$('.intro,.section-1,.section-2,.section-3,.section-4,.section-5').addClass('fade-out');
-$('.intro,.section-1,.section-2,.section-3,.section-4,.section-5').addClass('transition');
-
-
+$('.intro,.section-1,.section-2,.section-3,.section-4,.section-5').addClass(
+	'fade-out'
+)
+$('.intro,.section-1,.section-2,.section-3,.section-4,.section-5').addClass(
+	'transition'
+)
 
 window.onscroll = function change() {
-
-   array.forEach(element => {
-      if (window.scrollY>offset(element)-480) {
-      element.classList.remove('fade-out')
-      array.shift(); //removes the element from the array to reduce overhead
-   }
-})    
-   
+	array.forEach((element) => {
+		if (window.scrollY > offset(element) - 480) {
+			element.classList.remove('fade-out')
+			array.shift() //removes the element from the array to reduce overhead
+		}
+	})
 }
